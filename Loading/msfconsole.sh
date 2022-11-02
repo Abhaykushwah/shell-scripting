a64a9bd66edfa9232d4506394db444e502bd0298
Abhay Kushwah <67948225+Abhaykushwah@users.noreply.github.com>
2022-11-02 16:32:32 +0530

    Loading animation of msfconsole

---
+++ Loading/msfconsole.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "Please Wait while Loading..."
+sleep 0.4
+
+echo "Loading date - ";
+sleep 0.2
+clear
+echo "lOading date \ ";
+sleep 0.2
+clear
+echo "loAding date | ";
+sleep 0.2
+clear
+echo "loaDing date / ";
+sleep 0.2
+clear
+echo "loadIng date - ";
+sleep 0.3
+clear
+echo "loadiNg date \ ";
+sleep 0.2
+clear
+echo "loadinG date | ";
+sleep 0.2
+clear
+echo "loading Date / ";
+sleep 0.2
+clear
+echo "loading dAte - ";
+sleep 0.2
+clear
+echo "loading daTe";
+sleep 0.2
+clear
+echo "loading datE";
+sleep 0.7
+echo "Date Loaded Successfully "
+date
